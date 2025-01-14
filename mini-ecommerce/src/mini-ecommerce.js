cb34d344ffa2a4b2e66c22c3080455e2c8b6c6cb
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-14 13:23:49 -0300

    Criação dos useState

--- mini-ecommerce/src/mini-ecommerce.js
+++ mini-ecommerce/src/mini-ecommerce.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './mini-ecommerce.css';
 
 function MiniEcommerce() {
+
+  const [carrinho, setCarrinho] = useState({ produtos: []});
+  const [exibirProduto, setExibirProduto] = useState(true);
+  const [exibirCheckout, setExibirCheckout] = useState(true);
+  const [total, setTotal] = useState('0,00');
+
   return (
      <h1>Mini ecommerce</h1>
   );
