31a185ed86ed9c6cfdfc4fcb4788ee7b79a5cc97
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-15 11:22:28 -0300

    importação dos componentes do bootstrap

--- mini-ecommerce/src/components/produtos/listar-produtos.js
+++ mini-ecommerce/src/components/produtos/listar-produtos.js
@@ -1,5 +1,7 @@
 import React from "react";
 import placeholder from "../../images/placeholder.jpg";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button"
 
 function ListarProdutos() {
     return (<h1>Listar</h1>);
