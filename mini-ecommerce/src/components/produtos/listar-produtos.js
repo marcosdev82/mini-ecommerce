77287c50cb044444fab3b1ffd92bf663aeda757d
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-15 11:38:53 -0300

    Criação do render card

--- mini-ecommerce/src/components/produtos/listar-produtos.js
+++ mini-ecommerce/src/components/produtos/listar-produtos.js
@@ -1,10 +1,42 @@
 import React from "react";
 import placeholder from "../../images/placeholder.jpg";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 
 function ListarProdutos() {
-    return (<h1>Listar</h1>);
+    const produtos = [
+        { nome: "Curso de JavaScript Básico", preco: 'R$ 99.90' },
+        { nome: "Curso de React Avançado", preco: 'R$ 149.90' },
+        { nome: "Curso de Node.js Completo", preco: 'R$ 129.90' },
+        { nome: "Curso de Python para Iniciantes", preco: 'R$ 89.90' },
+        { nome: "Curso de Desenvolvimento Mobile com React Native", preco: 'R$ 199.90' },
+        { nome: "Curso de Design Responsivo com CSS", preco: 'R$ 79.90' },
+        { nome: "Curso de Inteligência Artificial com Python", preco: 'R$ 249.90' }
+    ];
+
+    function render() {
+        <Card
+            style={{ width: '18rem', margin: '10px', float: 'left' }}>
+            <Card.Img variant="top" src={placeholder} />
+            <Card.Body className="text-center">
+                <Card.Title style={{ height: '40px' }}>
+                    {/** nome do produto */}
+                </Card.Title>
+                <Card.Text>
+                    Descrição do produto aqui...
+                </Card.Text>
+                <Button
+                    variant="success"
+                    style={{ width: '100%' }}
+                    onClick={}>
+                    Comprar {{/** Preço do produto */}}
+                </Button>
+            </Card.Body>
+            
+        </Card>
+    }
+      
+    return render();
 }
 
 export default ListarProdutos;
