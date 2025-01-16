f9e9fd6ce271a0e1c7e75e2fa2fba5dc78b4609f
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-16 13:32:23 -0300

    corrigindo ternária props.produtos.length

--- mini-ecommerce/src/components/menu/menu.js
+++ mini-ecommerce/src/components/menu/menu.js
@@ -3,44 +3,45 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faShoppingBasket, faCashRegister, faShoppingCart
-} from '@fortawesome/free-solid-svg-icons'
+} from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
 function Menu(props) {
+   
     return (
-        <Navbar bg="dark" variant="dark" style={{padding: '5px'}}>
-            <Navbar.Brand href="">Mini Ecommerce</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" style={{ padding: '5px' }}>
+            <Navbar.Brand href="#">Mini Ecommerce</Navbar.Brand>
             <Navbar.Collapse className="justify-content-end">
                 <Nav>
                     <NavDropdown
                         title={
-                            <div style={{ display: "inline-block"}}>
+                            <div style={{ display: "inline-block" }}>
                                 <FontAwesomeIcon icon={faShoppingCart} />
                                 &nbsp;
                                 Carrinho
                             </div>
                         }
-                        drop="down" 
+                        drop="down"
                         align="end"
-                    >
-                        <NavDropdown.Item href=""
+                        >
+                        <NavDropdown.Item href="#"
                             onClick={props.handleExibirProdutos}
-                            >
+                        >
                             <FontAwesomeIcon icon={faShoppingBasket} />
                             &nbsp;
                             <strong>Produtos</strong>
                         </NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="">
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#">
                             &nbsp;
-                            Total: R$ {/** Chamar a função de calculo do total */}
+                            Total: R$ {/** Chamar a função de cálculo do total */}
                         </NavDropdown.Item>
-                        <span className={ props.produtos.length === 0 ? 'hidden' : null }> 
-                            <NavDropdown.Divider/>
+                        <span className={props.produtos && props.produtos.length === 0 ? 'hidden' : null}>
+                            <NavDropdown.Divider />
                             <NavDropdown.Item
-                                href=""
-                                style={{color: 'green'}}
-                                onClick={ props.handleExibirCheckout }
+                                href="#"
+                                style={{ color: 'green' }}
+                                onClick={props.handleExibirCheckout}
                             >
                                 <FontAwesomeIcon icon={faCashRegister} />
                                 &nbsp;
@@ -58,6 +59,6 @@ Menu.propTypes = {
     produtos: PropTypes.array.isRequired,
     handleExibirProdutos: PropTypes.func.isRequired,
     handleExibirCheckout: PropTypes.func.isRequired
-}
+};
 
 export default Menu;
