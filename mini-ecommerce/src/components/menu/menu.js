7cdfbcb4f65a8b80aec667070fb4113b2982ee3d
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-16 12:45:41 -0300

    Implementação do drop down

--- mini-ecommerce/src/components/menu/menu.js
+++ mini-ecommerce/src/components/menu/menu.js
@@ -7,7 +7,47 @@ import {
 
 function Menu() {
     return (
-        <h1>Menu</h1>
+        <Navbar bg="dark" variant="dark" style={{padding: '5px'}}>
+            <Navbar.Brand href="">Mini Ecommerce</Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+                <Nav>
+                    <NavDropdown
+                        title={
+                            <div style={{ display: "inline-block"}}>
+                                <FontAwesomeIcon icon={faShoppingCart} />
+                                &nbsp;
+                                Carrinho
+                            </div>
+                        }
+                        drop="down" 
+                        align="end"
+                    >
+                        <NavDropdown.Item href="">
+                            <FontAwesomeIcon icon={faShoppingBasket} />
+                            &nbsp;
+                            <strong>Produtos</strong>
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider/>
+                        <NavDropdown.Item href="">
+                           
+                            &nbsp;
+                            Total: R$ {/** Chamar a função de calculo do total */}
+                        </NavDropdown.Item>
+                        <span> 
+                            <NavDropdown.Divider/>
+                            <NavDropdown.Item
+                                href=""
+                                style={{color: 'green'}}
+                            >
+                                <FontAwesomeIcon icon={faCashRegister} />
+                                &nbsp;
+                                Finalizar compra
+                            </NavDropdown.Item>
+                        </span>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 export default Menu;
