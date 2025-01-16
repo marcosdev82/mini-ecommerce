563c74c27fe3ec029e03e05737626d85fab752db
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-16 08:57:34 -0300

    Importação das dependencias

--- mini-ecommerce/src/components/menu/menu.js
+++ mini-ecommerce/src/components/menu/menu.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faShoppingBasket, faCashRegister, faShoppingCart
+} from '@fortawesome/free-solid-svg-icons'
 
 function Menu() {
     return (
