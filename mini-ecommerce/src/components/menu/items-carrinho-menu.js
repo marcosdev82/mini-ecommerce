bbf2dc4f78b6568c8faefef10ea446c85d4cc413
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-17 13:02:30 -0300

    Verifica carrinho vazil

--- mini-ecommerce/src/components/menu/items-carrinho-menu.js
+++ mini-ecommerce/src/components/menu/items-carrinho-menu.js
@@ -1,8 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { NavDropdown } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSadTear } from "@fortawesome/free-solid-svg-icons";
 
 function ItensCarrinhoMenu(props) {
-    return();
+
+    function render() {
+
+        if (!props.produtos && props.produtos.length === 0) {
+            <NavDropdown.Item href="#" data-testid="items">
+                <FontAwesomeIcon icon={faSadTear}>
+                    &npsp;
+                    Carrinho vazil...
+                </FontAwesomeIcon>
+            </NavDropdown.Item>
+        }
+    }
+
+    return render();
 }
 
 ItensCarrinhoMenu.prototypeS = {
